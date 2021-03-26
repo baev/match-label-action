f3541d81ed00e0ec4ec522beecb2d78576c7e273
Dmitry Baev <baev.dm@gmail.com>
2021-03-26 13:23:13 +0300

    add debug output

--- index.js
+++ index.js
@@ -7,19 +7,25 @@ function run() {
     const pr = context.payload.pull_request || {}
     const labels = pr.labels || []
     const labelNames = labels.map(label => label.name)
+    core.debug(`pull request has ${labelNames.length} labels:`)
+    labelNames.forEach(ln => core.debug(`label [${ln}]`))
     const allowedLabels = match.parseAllowed(core.getInput('allowed'))
+    core.debug(`allowed labels:`)
+    allowedLabels.forEach(al => core.debug(`label [${al}]`))
     const allowedMultipleLabels = match.parseAllowed(
       core.getInput('allowed_multiple')
     )
     let matchingLabel
     if (allowedLabels.length > 0) {
       matchingLabel = match.findMatching(labelNames, allowedLabels, false)
+      core.debug(`found matching label (allowed branch): ${matchingLabel}`)
     } else if (allowedMultipleLabels.length > 0) {
       matchingLabel = match.findMatching(
         labelNames,
         allowedMultipleLabels,
         true
       )
+      core.debug(`found matching label (allowed allowed_multiple): ${matchingLabel}`)
     } else {
       return core.setFailed(
         'You must provide either `allowed` or `allowed_multiple` as input.'
